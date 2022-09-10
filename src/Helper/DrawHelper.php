ee898675f63a7d14d6ae15a13ce13a871ad5b4c4
Bilel <b.oueslati@ewm.swiss>
2022-09-10 04:37:01 +0200

    Update Draws Helper

--- src/Helper/DrawHelper.php
+++ src/Helper/DrawHelper.php
@@ -7,23 +7,31 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 class DrawHelper
 {
 
+    private array $draws;
+
     public function __construct(ParameterBagInterface $bag)
     {
-
         $this->bag = $bag;
+
+        $this->init();
     }
- 
+
     /**
      * Get draws from json file and parse it as array
      */
-     
-    public function getDraws(): array
+    public function init(): void
     {
         $publicPath = $this->bag->get("kernel.project_dir");
 
         $draws = json_decode(file_get_contents($publicPath . "/public/assets/draws.json"), true);
 
-        return $draws;
+        $this->setDraws($draws);
+    }
+
+    public function getDraws(): array
+    {
+
+        return $this->draws;
     }
 
     /**
@@ -33,7 +41,7 @@ class DrawHelper
     public function filter(string $type): array
     {
 
-        $results = $this->getDraws()["results"];
+        $results = $this->draws["results"];
 
         return array_values(array_filter($results, fn ($result) => $result["type"] == $type));
     }
@@ -47,12 +55,19 @@ class DrawHelper
 
         $draws = $this->filter($type);
 
-        $values =  array_map(fn($el) => $el["value"], $draws);
+        $values =  array_map(fn ($el) => $el["value"], $draws);
 
         return array_reduce(
             $values,
             fn ($prev, $curr) => $prev + $curr,
             0
         );
+    }
+
+    public function setDraws(array $draws)
+    {
+        $this->draws = $draws;
+
+        return $this;
     }
 }
